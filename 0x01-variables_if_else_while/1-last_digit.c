cfa63aa4816f2bda105f18ac37244f43ec95459e
AshaOladayo <ashayeoladayo@gmail.com>
2023-09-08 15:00:00 -0700

    Done

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,16 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * main - prints all alphabets in reverse
- * Return: returns 0
+ * main - function to check the size of last digit of random
+ * number
+ *
+ * Return: 0
  */
+
 int main(void)
 {
-	int i;
+	int n, l;
 
-	for (i = 'z'; i >= 'a'; i--)
-		putchar(i);
-	putchar('\n');
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	l = n % 10;
+
+	if (l > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, l);
+	}
+	else if (l == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, l);
+	}
+	else if (l < 6 && l != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+	}
 
 	return (0);
 }
